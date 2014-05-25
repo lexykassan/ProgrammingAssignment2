ef7f00eaf9e22dbcf2ff318d3eebc3232a5932db
lexykassan <lexykassan@gmail.com>
2014-05-25 01:57:01 -0400

    Adding baseline functions & comments
    Added the start of the calculations for the two functions. Added
    comments regarding what each portion does.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function generates the inverse of a matrix called as an argument
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmatrix <- function(solve) m <<- solve
+  getmatrix <- function() m
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
 }
 
-
-## Write a short comment describing this function
+## This function looks for a previously cached matrix inverse that was calculated and returns that if available
+## If not found, it calculates and returns the inverse of the matrix specified as an argument
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getmatrix()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setmatrix(m)
+  m
 }
